857a3af1ffe1f06a806b9a20d576b040b8cb247a
Øystein Walle <oystwa@gmail.com>
2023-06-16 15:53:15 +0200

    kek

--- culpa/tests/common/mod.rs
+++ culpa/tests/common/mod.rs
@@ -3,7 +3,7 @@ use pretty_assertions::assert_eq;
 
 pub const FILE: &str = "lorem-ipsum.txt";
 
-// Return list of strings in the format "SHA1 SP <line contents>"
+// Return list of strings in the format "SHA1 SP true/false SP <line contents>"
 pub fn run_git_blame(revision: &str, args: &[&str]) -> Vec<String> {
     let output = std::process::Command::new("git")
         .args(["-C", "..", "blame"])
@@ -14,6 +14,36 @@ pub fn run_git_blame(revision: &str, args: &[&str]) -> Vec<String> {
         .stdout;
 
     let output = std::str::from_utf8(&output).expect("valid UTF-8");
+
+    // Example output:
+    //
+    // 00 753d1dba0c677cdb2f32be664faaff55856ede66 4 4
+    // 01 author Øystein Walle
+    // 02 author-mail <you@example.com>
+    // 03 author-time 1678823979
+    // 04 author-tz +0100
+    // 05 committer Øystein Walle
+    // 06 committer-mail <oystwa@gmail.com>
+    // 07 committer-time 1685450005
+    // 08 committer-tz +0200
+    // 09 summary Initial commit
+    // 10 boundary
+    // 11 filename lorem-ipsum.txt
+    // 12         fermentum quam, varius vestibulum est nisi et ex. Sed luctus est eu odio
+    //
+    // 00 1050bf853e2209915f1864c3d7c9a22c599f8dc8 5 5 2
+    // 01 author Øystein Walle
+    // 02 author-mail <oystwa@gmail.com>
+    // 03 author-time 1685440586
+    // 04 author-tz +0200
+    // 05 committer Øystein Walle
+    // 06 committer-mail <oystwa@gmail.com>
+    // 07 committer-time 1685450082
+    // 08 committer-tz +0200
+    // 09 summary Multiple changes in one commit again
+    // 10 previous 392db1bdc8269623a3261e7340bfca3e61e209f4 lorem-ipsum.txt
+    // 11 filename lorem-ipsum.txt
+    // 12         Spenol er best i verden!
 
     output
         .split_terminator('\n')
