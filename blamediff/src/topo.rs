afa88ea2fb7df72c0285b5b0df01f4ec7f2ef2f0
Øystein Walle <oystwa@gmail.com>
2023-10-07 00:26:36 +0200

    WIP external find

--- blamediff/src/topo.rs
+++ blamediff/src/topo.rs
@@ -1,6 +1,7 @@
 use gix_commitgraph::Graph;
-use gix_hash::ObjectId;
+use gix_hash::{oid, ObjectId};
 use gix_hashtable::hash_map::Entry;
+use gix_odb::FindExt;
 use gix_revwalk::{graph::IdMap, PriorityQueue};
 
 use flagset::{flags, FlagSet};
@@ -39,7 +40,8 @@ pub enum Error {
 }
 
 /// A commit walker that walks in topographical order, like `git rev-list --topo-order`.
-pub struct TopoWalker {
+pub struct TopoWalker<'repo> {
+    repo: &'repo gix::Repository,
     commit_graph: Graph,
     indegrees: IdMap<i32>,
     states: IdMap<WalkState>,
@@ -47,19 +49,21 @@ pub struct TopoWalker {
     indegree_queue: PriorityQueue<u32, ObjectId>,
     topo_queue: Vec<ObjectId>,
     min_gen: u32,
+    buf: Vec<u8>,
 }
 
 // #[trace(disable(on_repo))]
-impl<'a> TopoWalker {
+impl<'repo> TopoWalker<'repo> {
     /// Create a new TopoWalker that walks the given repository, starting at the
     /// tips and ending at the bottoms. Like `git rev-list --topo-order
     /// ^bottom... tips...`
     pub fn on_repo(
-        repo: &'a gix::Repository,
+        repo: &'repo gix::Repository,
         tips: impl IntoIterator<Item = impl Into<ObjectId>>,
         bottoms: impl IntoIterator<Item = impl Into<ObjectId>>,
     ) -> Result<Self, Error> {
         let mut s = Self {
+            repo,
             commit_graph: repo.commit_graph()?,
             indegrees: IdMap::default(),
             states: IdMap::default(),
@@ -67,6 +71,7 @@ impl<'a> TopoWalker {
             indegree_queue: PriorityQueue::new(),
             topo_queue: vec![],
             min_gen: gix_commitgraph::GENERATION_NUMBER_INFINITY,
+            buf: vec![],
         };
 
         let tips = tips.into_iter().map(Into::into).collect::<Vec<_>>();
@@ -82,11 +87,15 @@ impl<'a> TopoWalker {
         {
             *s.indegrees.entry(*id).or_default() = 1;
 
-            let gen = s
-                .commit_graph
-                .commit_by_id(id)
-                .ok_or(Error::CommitNotFound)?
-                .generation();
+            let gen = match find(
+                &s.commit_graph,
+                |id, buf| s.repo.objects.find_commit_iter(id, buf),
+                id,
+                &mut s.buf,
+            ) {
+                Either::CommitRefIter(c) => gix_commitgraph::GENERATION_NUMBER_INFINITY,
+                Either::CachedCommit(c) => c.generation(),
+            };
 
             if gen < s.min_gen {
                 s.min_gen = gen;
@@ -131,15 +140,14 @@ impl<'a> TopoWalker {
         if let Some((gen, id)) = self.indegree_queue.pop() {
             self.explore_to_depth(gen)?;
 
-            let commit = self
-                .commit_graph
-                .commit_by_id(id)
-                .ok_or(Error::CommitNotFound)?;
+            let pgen = get_parent_generations(
+                &self.commit_graph,
+                |id, buf| self.repo.objects.find_commit_iter(id, buf),
+                &id,
+                &mut self.buf,
+            );
 
-            for p in commit.iter_parents() {
-                let parent_commit = self.commit_graph.commit_at(p.expect("get position"));
-                let pid = ObjectId::from(parent_commit.id());
-
+            for (pid, gen) in pgen {
                 self.indegrees
                     .entry(pid)
                     .and_modify(|e| *e += 1)
@@ -149,7 +157,7 @@ impl<'a> TopoWalker {
 
                 if !state.0.contains(WalkFlags::InDegree) {
                     state.0 |= WalkFlags::InDegree;
-                    self.indegree_queue.insert(parent_commit.generation(), pid);
+                    self.indegree_queue.insert(gen, pid);
                 }
             }
         }
@@ -173,22 +181,19 @@ impl<'a> TopoWalker {
         if let Some((_, id)) = self.explore_queue.pop() {
             self.process_parents(id)?;
 
-            let commit = self
-                .commit_graph
-                .commit_by_id(id)
-                .ok_or(Error::CommitNotFound)?;
+            let pgen = get_parent_generations(
+                &self.commit_graph,
+                |id, buf| self.repo.objects.find_commit_iter(id, buf),
+                &id,
+                &mut self.buf,
+            );
 
-            for p in commit.iter_parents() {
-                let parent_commit = self.commit_graph.commit_at(p?);
-                let state = self
-                    .states
-                    .get_mut(parent_commit.id())
-                    .ok_or(Error::MissingState)?;
+            for (pid, gen) in pgen {
+                let state = self.states.get_mut(&pid).ok_or(Error::MissingState)?;
 
                 if !state.0.contains(WalkFlags::Explored) {
                     state.0 |= WalkFlags::Explored;
-                    self.explore_queue
-                        .insert(parent_commit.generation(), parent_commit.id().into());
+                    self.explore_queue.insert(gen, pid.into());
                 }
             }
         }
@@ -197,19 +202,16 @@ impl<'a> TopoWalker {
     }
 
     fn expand_topo_walk(&mut self, id: ObjectId) -> Result<(), Error> {
-        let parents = self
-            .commit_graph
-            .commit_by_id(id)
-            .ok_or(Error::CommitNotFound)?
-            .iter_parents()
-            .collect::<Result<Vec<_>, _>>()?;
-
         self.process_parents(id)?;
 
-        for p in parents {
-            let parent_gen = self.commit_graph.commit_at(p).generation();
-            let pid = ObjectId::from(self.commit_graph.commit_at(p).id());
+        let pgen = get_parent_generations(
+            &self.commit_graph,
+            |id, buf| self.repo.objects.find_commit_iter(id, buf),
+            &id,
+            &mut self.buf,
+        );
 
+        for (pid, parent_gen) in pgen {
             let parent_flags = self.states.get(&pid).ok_or(Error::MissingState)?;
 
             if parent_flags.0.contains(WalkFlags::Uninteresting) {
@@ -249,16 +251,14 @@ impl<'a> TopoWalker {
             state.0 & WalkFlags::Uninteresting
         };
 
-        let commit = self
-            .commit_graph
-            .commit_by_id(id)
-            .ok_or(Error::CommitNotFound)?;
+        let pgen = get_parent_generations(
+            &self.commit_graph,
+            |id, buf| self.repo.objects.find_commit_iter(id, buf),
+            &id,
+            &mut self.buf,
+        );
 
-        for p in commit.iter_parents() {
-            let parent_commit = self.commit_graph.commit_at(p?);
-
-            let pid = ObjectId::from(parent_commit.id());
-
+        for (pid, _) in pgen {
             match self.states.entry(pid) {
                 Entry::Occupied(mut o) => o.get_mut().0 |= pass_flags,
                 Entry::Vacant(v) => {
@@ -271,8 +271,61 @@ impl<'a> TopoWalker {
     }
 }
 
+fn get_parent_generations<'b, 'g, Find, E>(
+    commit_graph: &'g gix_commitgraph::Graph,
+    f: Find,
+    id: &oid,
+    buf: &'b mut Vec<u8>,
+) -> Vec<(ObjectId, u32)>
+where
+    Find:
+        for<'a> FnMut(&gix_hash::oid, &'a mut Vec<u8>) -> Result<gix_object::CommitRefIter<'a>, E>,
+    E: std::error::Error + Send + Sync + 'static,
+{
+    let mut pgen = vec![];
+
+    match find(commit_graph, f, &id, buf) {
+        Either::CommitRefIter(c) => {
+            for token in c {
+                match token {
+                    Ok(gix_object::commit::ref_iter::Token::Parent { id }) => {
+                        pgen.push((id, gix_commitgraph::GENERATION_NUMBER_INFINITY));
+                    }
+                    _ => continue,
+                }
+            }
+        }
+        Either::CachedCommit(c) => {
+            for p in c.iter_parents() {
+                let parent_commit = commit_graph.commit_at(p.expect("get position"));
+                let pid = ObjectId::from(parent_commit.id());
+                pgen.push((pid, parent_commit.generation()));
+            }
+        }
+    };
+
+    pgen
+}
+
+fn find<'b, 'g, Find, E>(
+    commit_graph: &'g gix_commitgraph::Graph,
+    mut find: Find,
+    id: &oid,
+    buf: &'b mut Vec<u8>,
+) -> Either<'b, 'g>
+where
+    Find:
+        for<'a> FnMut(&gix_hash::oid, &'a mut Vec<u8>) -> Result<gix_object::CommitRefIter<'a>, E>,
+    E: std::error::Error + Send + Sync + 'static,
+{
+    match commit_graph.commit_by_id(id).map(Either::CachedCommit) {
+        Some(c) => c,
+        None => (find)(id, buf).map(Either::CommitRefIter).unwrap(),
+    }
+}
+
 // #[trace]
-impl Iterator for TopoWalker {
+impl Iterator for TopoWalker<'_> {
     type Item = Result<ObjectId, Error>;
 
     fn next(&mut self) -> Option<Self::Item> {
@@ -296,6 +349,11 @@ impl Iterator for TopoWalker {
 
         Some(Ok(id))
     }
+}
+
+enum Either<'buf, 'cache> {
+    CommitRefIter(gix_object::CommitRefIter<'buf>),
+    CachedCommit(gix_commitgraph::file::Commit<'cache>),
 }
 
 #[cfg(test)]
