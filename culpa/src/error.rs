6c0af5cda710a400d037f39b8f36584db56ff2dc
Øystein Walle <oystwa@gmail.com>
2023-12-04 08:22:27 +0100

    culpa: use thiserror

--- culpa/src/error.rs
+++ culpa/src/error.rs
@@ -1,51 +1,26 @@
 /// The error produced if a complete blame cannot be produced.
-#[derive(Debug)]
+#[derive(thiserror::Error, Debug)]
 pub enum Error {
+    #[error("lol")]
     Generation,
+    #[error("lol")]
     InvalidRange,
-    NotFound(std::io::Error),
-    PeelError(gix::object::peel::to_kind::Error),
-    FindObject(gix::odb::find::existing::Error),
-    SystemTime(std::time::SystemTimeError),
-    ParseSingle(gix::revision::spec::parse::single::Error),
-    Parse(gix::revision::spec::parse::Error),
-    StrUtf8Error(std::str::Utf8Error),
-    WalkError(gix::revision::walk::Error),
+    #[error("lol")]
+    NotFound(#[from] std::io::Error),
+    #[error("lol")]
+    PeelError(#[from] gix::object::peel::to_kind::Error),
+    #[error("lol")]
+    FindObject(#[from] gix::object::find::existing::Error),
+    #[error("lol")]
+    SystemTime(#[from] std::time::SystemTimeError),
+    #[error("lol")]
+    ParseSingle(#[from] gix::revision::spec::parse::single::Error),
+    #[error("lol")]
+    Parse(#[from] gix::revision::spec::parse::Error),
+    #[error("lol")]
+    StrUtf8Error(#[from] std::str::Utf8Error),
+    #[error("lol")]
+    WalkError(#[from] gix::revision::walk::Error),
+    #[error("lol")]
+    TopoError(#[from] topo::Error),
 }
-
-impl std::fmt::Display for Error {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Error::Generation => write!(f, "Internal generation error"),
-            Error::InvalidRange => write!(f, "Invalid range provided"),
-            Error::NotFound(e) => write!(f, "Path not found in tree: {}", e),
-            Error::PeelError(e) => write!(f, "Error finding blob in object: {}", e),
-            Error::FindObject(e) => write!(f, "Error finding object: {}", e),
-            Error::ParseSingle(e) => write!(f, "Error parsing single revision: {}", e),
-            Error::Parse(e) => write!(f, "Error parsing revision: {}", e),
-            Error::StrUtf8Error(e) => write!(f, "Error converting data to string: {}", e),
-            Error::WalkError(e) => write!(f, "Error walking the history: {}", e),
-            Error::SystemTime(e) => write!(f, "Error finding timestamp: {}", e),
-        }
-    }
-}
-
-impl std::error::Error for Error {}
-
-macro_rules! make_error {
-    ($e:ty, $b:ident) => {
-        impl From<$e> for Error {
-            fn from(e: $e) -> Self {
-                Error::$b(e)
-            }
-        }
-    };
-}
-make_error![std::time::SystemTimeError, SystemTime];
-make_error![gix::revision::spec::parse::single::Error, ParseSingle];
-make_error![gix::revision::spec::parse::Error, Parse];
-make_error![gix::odb::find::existing::Error, FindObject];
-make_error![gix::object::peel::to_kind::Error, PeelError];
-make_error![std::str::Utf8Error, StrUtf8Error];
-make_error![gix::revision::walk::Error, WalkError];
-make_error![std::io::Error, NotFound];
